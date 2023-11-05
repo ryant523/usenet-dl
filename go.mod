0daa92eb703a39b9dfd9f41170e64d0576db258f
Ryan Toole <ryant523@gmail.com>
2023-11-05 14:33:32 -0500

    First Commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/ryant523/usenet-dl
+
+go 1.21.3
---
+++ tmdb/api.go
@@ -0,0 +1,203 @@
+package tmdb
+
+import (
+	"encoding/json"
+	"io"
+	"net/http"
+	"net/url"
+	"strconv"
+)
+
+const baseUrl = "https://api.themoviedb.org/3/"
+
+type SearchResults struct {
+	Page         int      `json:"page"`
+	Results      []Result `json:"results"`
+	TotalPages   int      `json:"total_pages"`
+	TotalResults int      `json:"total_results"`
+}
+
+type Result struct {
+	Adult            bool     `json:"adult"`
+	BackdropPath     string   `json:"backdrop_path"`
+	GenreIds         []int    `json:"genre_ids"`
+	ID               int      `json:"id"`
+	OriginCountry    []string `json:"origin_country"`
+	OriginalLanguage string   `json:"original_language"`
+	OriginalName     string   `json:"original_name"`
+	Overview         string   `json:"overview"`
+	Popularity       float64  `json:"popularity"`
+	PosterPath       string   `json:"poster_path"`
+	ReleaseDate      string   `json:"release_date"`
+	FirstAirDate     string   `json:"first_air_date"`
+	Name             string   `json:"name"`
+	Title            string   `json:"title"`
+	Video            bool     `json:"video"`
+	VoteAverage      float64  `json:"vote_average"`
+	VoteCount        int      `json:"vote_count"`
+}
+
+type Movie struct {
+	Adult               bool   `json:"adult"`
+	BackdropPath        string `json:"backdrop_path"`
+	BelongsToCollection struct {
+		ID           int    `json:"id"`
+		Name         string `json:"name"`
+		PosterPath   string `json:"poster_path"`
+		BackdropPath string `json:"backdrop_path"`
+	} `json:"belongs_to_collection"`
+	Budget int `json:"budget"`
+	Genres []struct {
+		ID   int    `json:"id"`
+		Name string `json:"name"`
+	} `json:"genres"`
+	Homepage            string  `json:"homepage"`
+	ID                  int     `json:"id"`
+	ImdbID              string  `json:"imdb_id"`
+	OriginalLanguage    string  `json:"original_language"`
+	OriginalTitle       string  `json:"original_title"`
+	Overview            string  `json:"overview"`
+	Popularity          float64 `json:"popularity"`
+	PosterPath          string  `json:"poster_path"`
+	ProductionCompanies []struct {
+		ID            int    `json:"id"`
+		LogoPath      string `json:"logo_path"`
+		Name          string `json:"name"`
+		OriginCountry string `json:"origin_country"`
+	} `json:"production_companies"`
+	ProductionCountries []struct {
+		Iso31661 string `json:"iso_3166_1"`
+		Name     string `json:"name"`
+	} `json:"production_countries"`
+	ReleaseDate     string `json:"release_date"`
+	Revenue         int    `json:"revenue"`
+	Runtime         int    `json:"runtime"`
+	SpokenLanguages []struct {
+		EnglishName string `json:"english_name"`
+		Iso6391     string `json:"iso_639_1"`
+		Name        string `json:"name"`
+	} `json:"spoken_languages"`
+	Status      string  `json:"status"`
+	Tagline     string  `json:"tagline"`
+	Title       string  `json:"title"`
+	Video       bool    `json:"video"`
+	VoteAverage float64 `json:"vote_average"`
+	VoteCount   int     `json:"vote_count"`
+}
+
+type TvShow struct {
+	Adult        bool   `json:"adult"`
+	BackdropPath string `json:"backdrop_path"`
+	CreatedBy    []struct {
+		ID          int    `json:"id"`
+		CreditID    string `json:"credit_id"`
+		Name        string `json:"name"`
+		Gender      int    `json:"gender"`
+		ProfilePath string `json:"profile_path"`
+	} `json:"created_by"`
+	EpisodeRunTime []int  `json:"episode_run_time"`
+	FirstAirDate   string `json:"first_air_date"`
+	Genres         []struct {
+		ID   int    `json:"id"`
+		Name string `json:"name"`
+	} `json:"genres"`
+	Homepage         string   `json:"homepage"`
+	ID               int      `json:"id"`
+	InProduction     bool     `json:"in_production"`
+	Languages        []string `json:"languages"`
+	LastAirDate      string   `json:"last_air_date"`
+	LastEpisodeToAir struct {
+		ID             int     `json:"id"`
+		Name           string  `json:"name"`
+		Overview       string  `json:"overview"`
+		VoteAverage    float64 `json:"vote_average"`
+		VoteCount      int     `json:"vote_count"`
+		AirDate        string  `json:"air_date"`
+		EpisodeNumber  int     `json:"episode_number"`
+		EpisodeType    string  `json:"episode_type"`
+		ProductionCode string  `json:"production_code"`
+		Runtime        int     `json:"runtime"`
+		SeasonNumber   int     `json:"season_number"`
+		ShowID         int     `json:"show_id"`
+		StillPath      string  `json:"still_path"`
+	} `json:"last_episode_to_air"`
+	Name             string      `json:"name"`
+	NextEpisodeToAir interface{} `json:"next_episode_to_air"`
+	Networks         []struct {
+		ID            int    `json:"id"`
+		LogoPath      string `json:"logo_path"`
+		Name          string `json:"name"`
+		OriginCountry string `json:"origin_country"`
+	} `json:"networks"`
+	NumberOfEpisodes    int      `json:"number_of_episodes"`
+	NumberOfSeasons     int      `json:"number_of_seasons"`
+	OriginCountry       []string `json:"origin_country"`
+	OriginalLanguage    string   `json:"original_language"`
+	OriginalName        string   `json:"original_name"`
+	Overview            string   `json:"overview"`
+	Popularity          float64  `json:"popularity"`
+	PosterPath          string   `json:"poster_path"`
+	ProductionCompanies []struct {
+		ID            int         `json:"id"`
+		LogoPath      interface{} `json:"logo_path"`
+		Name          string      `json:"name"`
+		OriginCountry string      `json:"origin_country"`
+	} `json:"production_companies"`
+	ProductionCountries []struct {
+		Iso31661 string `json:"iso_3166_1"`
+		Name     string `json:"name"`
+	} `json:"production_countries"`
+	Seasons []struct {
+		AirDate      string  `json:"air_date"`
+		EpisodeCount int     `json:"episode_count"`
+		ID           int     `json:"id"`
+		Name         string  `json:"name"`
+		Overview     string  `json:"overview"`
+		PosterPath   string  `json:"poster_path"`
+		SeasonNumber int     `json:"season_number"`
+		VoteAverage  float64 `json:"vote_average"`
+	} `json:"seasons"`
+	SpokenLanguages []struct {
+		EnglishName string `json:"english_name"`
+		Iso6391     string `json:"iso_639_1"`
+		Name        string `json:"name"`
+	} `json:"spoken_languages"`
+	Status      string  `json:"status"`
+	Tagline     string  `json:"tagline"`
+	Type        string  `json:"type"`
+	VoteAverage float64 `json:"vote_average"`
+	VoteCount   int     `json:"vote_count"`
+}
+
+type ExternalIds struct {
+	ID     int    `json:"id"`
+	ImdbID string `json:"imdb_id"`
+	TvDbID int    `json:"tvdb_id"`
+}
+
+func SearchMovie(apiKey string, query string, year int) (SearchResults, error) {
+	q := url.Values{}
+	q.Add("query", query)
+	if year != 0 {
+		q.Add("year", strconv.Itoa(year))
+	}
+
+	res, err := request(apiKey, "search/movie", q)
+	if err != nil {
+		return SearchResults{}, err
+	}
+	var movieResults SearchResults
+	err = json.Unmarshal([]byte(res), &movieResults)
+	return movieResults, err
+}
+
+func request(apiKey string, relativeUrl string, parameters url.Values) ([]byte, error) {
+	requestUrl, _ := url.Parse(baseUrl + relativeUrl)
+	parameters.Add("api_key", apiKey)
+	requestUrl.RawQuery = parameters.Encode()
+	res, err := http.Get(requestUrl.String())
+	if err != nil {
+		return nil, err
+	}
+	return io.ReadAll(res.Body)
+}
